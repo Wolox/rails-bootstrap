7184a40060fc65e7cc64d8ac8dbda522bf7996c4
Esteban Pintos <epintos7@gmail.com>
2013-12-30 19:13:02 -0300

    Added secret token initializer

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-RailsBootstrap::Application.config.secret_key_base = '29bbf58bc4b51d7b26b346fde7245db69d754f539c5d9e760f9664dc6572c369096aff7d31e19c26c70d1a39273808054604dba2c03a7a08c5daa3ec8b411984'
+security = AppConfiguration.for :security
+if security.secret_token.blank?
+  raise "secret_token has not been set. Create a .security.yml file with the secret_token or set the SECURITY_SECRET_KEY_BASE env variable"
+end
+RailsBootstrap::Application.config.secret_token = security.secret_token
