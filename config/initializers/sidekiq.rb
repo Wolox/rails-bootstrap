73d29182020ee452c9f3be61c89defc461fe735c
Esteban Pintos <esteban.pintos@wolox.com.ar>
2015-04-16 16:29:44 -0300

    Fix sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,7 @@
 
 require 'sidekiq'
 
-redis_url = "redis://#{ENV.fetch('REDIS_PORT_6379_TCP_ADDR', '127.0.0.1')}:6379" || ENV['REDIS_PROVIDER']
+redis_url = ENV['REDIS_PROVIDER'] || "redis://#{ENV.fetch('REDIS_PORT_6379_TCP_ADDR', '127.0.0.1')}:6379"
 Sidekiq.configure_server do |config|
   config.redis = {
     url: redis_url
