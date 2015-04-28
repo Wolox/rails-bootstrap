0dea16b1f7eb20799c7ed2e51b2fe7487542016e
Esteban Pintos <epintos7@gmail.com>
2015-04-28 12:33:54 -0300

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,18 +2,21 @@
 
 require 'sidekiq'
 
+url = ''
 if ENV['REDISCLOUD_URL']
-  uri = URI.parse(ENV['REDISCLOUD_URL'])
+  url = ENV['REDISCLOUD_URL']
 elsif ENV['REDISTOGO_URL']
-  uri = URI.parse(ENV['REDISCLOUD_URL'])
+  url = ENV['REDISCLOUD_URL']
 else
-  uri = URI.parse("redis://#{ENV.fetch('REDIS_1_PORT_6379_TCP_ADDR', '127.0.0.1')}:6379")
+  url = "redis://#{ENV.fetch('REDIS_1_PORT_6379_TCP_ADDR', '127.0.0.1')}:6379"
 end
 
-$redis_subscriber = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-$redis_publisher = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-
 Sidekiq.configure_server do |config|
-  config.redis = { size: 2 }
+  config.redis = { size: 2, url: url }
+  config.error_handlers << proc { |exception, context| Airbrake.notify_or_ignore(exception, parameters: context) }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: url }
   config.error_handlers << proc { |exception, context| Airbrake.notify_or_ignore(exception, parameters: context) }
 end
