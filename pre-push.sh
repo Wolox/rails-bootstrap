eb2de02540a19a8d3d319637cdb1849af026ef34
Esteban Pintos <esteban.pintos@wolox.com.ar>
2015-08-10 17:11:09 -0300

    Modified pre push hook

--- pre-push.sh
+++ pre-push.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
 
-echo 'Running Rspec tests'
+echo "\033[34mRunning Rspec tests\033[0m"
 RUN_CHECK_CMD='bundle exec rspec spec -fd'
 RUN_TESTS_OUTPUT=`${RUN_CHECK_CMD}`
 
 if [ $? -eq 1 ]
 then
-  echo "Can't commit! You've broken Rspec tests!!!"
+  echo "\033[34mCan't commit! You've broken Rspec tests!!!\033[0m"
   exit 1
 fi
 
-echo 'Running SCSS Lint'
+echo "\033[34mRunning SCSS Lint\033[0m"
 RUN_CHECK_CMD='bundle exec scss-lint app/assets/stylesheets/'
 RUN_TESTS_OUTPUT=`${RUN_CHECK_CMD}`
 
 if [ $? -eq 1 ]
 then
-  echo "Can't commit! You have scss lint offences!!!"
+  echo "\033[34mCan't commit! You have scss lint offences!!!\033[0m"
   exit 1
 fi
 
-echo 'Running Rubocop'
+echo "\033[34mRunning Rubocop\033[0m"
 RUN_CHECK_CMD='bundle exec rubocop app spec -R --format simple'
 RUN_TESTS_OUTPUT=`${RUN_CHECK_CMD}`
 
 if [ $? -eq 1 ]
 then
-  echo "Can't commit! You have rubocop offences!!!"
+  echo "\033[34mCan't commit! You have rubocop offences!!!\033[0m"
   exit 1
 fi
 
-echo "All checks passed. You didn't broke anything. Congrats!\n"
+echo "\033[34mAll checks passed. You didn't broke anything. Congrats!\n\033[0m"
 exit 0
