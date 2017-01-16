b38aa717a59a739b5bf140428d4a2ff2fb23c72d
Esteban Pintos <esteban.pintos@wolox.com.ar>
2017-01-16 11:41:38 -0300

    Updates 2.3.3

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '2.3.1'
+ruby '2.3.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
@@ -131,7 +131,7 @@ group :test do
 
   # CodeStats
   gem 'simplecov', require: false
-  gem 'codestats-metrics-reporter', '~> 0.1.8', require: nil
+  gem 'codestats-metrics-reporter', require: nil
   gem 'rubycritic', require: false
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150505151057) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
+
+  create_table "tolk_locales", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["name"], name: "index_tolk_locales_on_name", unique: true, using: :btree
+  end
+
+  create_table "tolk_phrases", force: :cascade do |t|
+    t.text     "key"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tolk_translations", force: :cascade do |t|
+    t.integer  "phrase_id"
+    t.integer  "locale_id"
+    t.text     "text"
+    t.text     "previous_text"
+    t.boolean  "primary_updated", default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["phrase_id", "locale_id"], name: "index_tolk_translations_on_phrase_id_and_locale_id", unique: true, using: :btree
+  end
+
 end
