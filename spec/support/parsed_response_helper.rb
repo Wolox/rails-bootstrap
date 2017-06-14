5ef8de117e5b4850f5f4ccffa992157758a6b872
Maor Levy <maor.levy@wolox.com.ar>
2017-06-14 15:41:36 -0300

    added restriction for parsed response to include an actual body

--- spec/support/parsed_response_helper.rb
+++ spec/support/parsed_response_helper.rb
@@ -1,7 +1,7 @@
 module Response
   module JSONParser
     def response_body
-      ActiveSupport::JSON.decode(response.body) if response.present?
+      ActiveSupport::JSON.decode(response.body) if response.present? && response.body.present?
     end
   end
 end
