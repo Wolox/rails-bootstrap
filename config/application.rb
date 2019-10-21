fca5e7c2e3568af6449a969aee03e3c9c9fe1234
Ignacio Coluccio <ignacio.coluccio@gmail.com>
2019-10-21 13:34:13 -0300

    Update to Rails 6

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'action_cable/engine'
 require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -17,31 +18,26 @@ Bundler.require(*Rails.groups)
 
 module RailsBootstrap
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    if Rails.application.secrets.email_recipients_interceptors.present?
-      Mail.register_interceptor RecipientInterceptor.new(
-        Rails.application.secrets.email_recipients_interceptors,
-        subject_prefix: '[INTERCEPTOR]'
-      )
-    end
-
-    # Tell your app to use the Rack::Attack middleware
-    config.middleware.use Rack::Attack
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins "#{ENV.fetch('CORS_PERMITTED_ORIGIN', '*')}"
-        resource "#{ENV.fetch('CORS_PERMITTED_ORIGIN', '*')}", headers: :any, methods: [:get, :post, :options]
-      end
-    end
-
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    # Renders eveything with Json as default (rails and ruby errors included).
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins ENV.fetch('CORS_PERMITTED_ORIGIN', '*').to_s
+        resource ENV.fetch('CORS_PERMITTED_ORIGIN', '*').to_s,
+                 expose: %w[access-token expiry token-type uid client],
+                 headers: :any,
+                 methods: %i[get post delete put patch options head]
+      end
+    end
   end
 end
