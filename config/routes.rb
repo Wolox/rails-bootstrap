0cc707088686719f92310826985a42df8907fa3a
Ignacio Coluccio <ignacio.coluccio@gmail.com>
2019-10-21 13:34:13 -0300

    Update to Rails 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'application#index'
-
-  mount Sidekiq::Web, at: 'sidekiq'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
